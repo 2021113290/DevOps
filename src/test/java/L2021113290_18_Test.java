7a28d4cd447ce84d264531fb8fa19efa41c47738
加油昕玉 <10162859+come-on-xinyu@user.noreply.gitee.com>
2023-12-10 20:57:36 +0800

    修改test

--- src/test/java/L2021113290_18_Test.java
+++ src/test/java/L2021113290_18_Test.java
@@ -1,62 +1,45 @@
-/*等价类划分原则：将输入域划分为不同的等价类，以确保覆盖各种可能的情况。
-方法的测试目的：在这个例子中，productExceptSelf 方法的主要目的是计算除自身以外数组中所有元素的乘积。因此，我们需要测试该方法是否能正确计算出这个结果，并覆盖边界情况。
-测试用例设计：我们可以设计以下测试用例来测试该方法：
-测试包含正整数的输入数组
-测试包含负整数的输入数组
-测试包含零的输入数组
-测试输入数组长度为1的情况
-测试输入数组长度为2的情况
-测试输入数组长度为3及以上的一般情况
+import org.junit.Assert;
+import org.junit.Test;
 
- */
 public class L2021113290_18_Test {
-    public static void main(String[] args) {
+    @Test
+    public void testProductExceptSelf() {
         Solution solution = new Solution();
-
-//
 
         // 测试用例1: 包含正整数的输入数组
         int[] nums2 = {1, 2, 3, 4};
+        int[] expected2 = {24, 12, 8, 6};
         int[] result2 = solution.productExceptSelf(nums2);
-        System.out.println("Test Case 1:");
-        printArray(result2);
+        Assert.assertArrayEquals(expected2, result2);
 
         // 测试用例2: 包含负整数的输入数组
         int[] nums3 = {1, -2, 3, -4};
+        int[] expected3 = {24, -12, 8, -6};
         int[] result3 = solution.productExceptSelf(nums3);
-        System.out.println("Test Case 2:");
-        printArray(result3);
+        Assert.assertArrayEquals(expected3, result3);
 
         // 测试用例3: 包含零的输入数组
         int[] nums4 = {1, 0, 3, 4};
+        int[] expected4 = {0, 12, 0, 0};
         int[] result4 = solution.productExceptSelf(nums4);
-        System.out.println("Test Case 3:");
-        printArray(result4);
+        Assert.assertArrayEquals(expected4, result4);
 
         // 测试用例4: 输入数组长度为1
         int[] nums5 = {2};
+        int[] expected5 = {1};
         int[] result5 = solution.productExceptSelf(nums5);
-        System.out.println("Test Case 4:");
-        printArray(result5);
+        Assert.assertArrayEquals(expected5, result5);
 
         // 测试用例5: 输入数组长度为2
         int[] nums6 = {2, 3};
+        int[] expected6 = {3, 2};
         int[] result6 = solution.productExceptSelf(nums6);
-        System.out.println("Test Case 5:");
-        printArray(result6);
+        Assert.assertArrayEquals(expected6, result6);
 
         // 测试用例6: 输入数组长度为3及以上的一般情况
         int[] nums7 = {1, 2, 3, 4, 5};
+        int[] expected7 = {120, 60, 40, 30, 24};
         int[] result7 = solution.productExceptSelf(nums7);
-        System.out.println("Test Case 6:");
-        printArray(result7);
-    }
-
-    // 辅助方法：打印数组
-    private static void printArray(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
+        Assert.assertArrayEquals(expected7, result7);
     }
 }
